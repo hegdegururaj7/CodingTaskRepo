1c369f798b07c10b3058b4192b903e68a4293e2d
GURURAJ HEGDE <hegdegururaj7@gmail.com>
2021-04-25 20:54:42 +0530

    search-results-table changes

---
+++ CodingTask/ClientApp/src/app/search-results/search-results.component.ts
@@ -0,0 +1,48 @@
+import { Movie } from "src/app/models/movie.model";
+import { Input, ViewChild, OnInit, Component } from "@angular/core";
+import { MatTableDataSource, MatSort, Sort } from "@angular/material/";
+
+@Component({
+  selector: 'app-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls :['./search-results.component.scss']
+}) 
+export class SearchResultsComponent implements OnInit {
+@Input('dataSource') searchResults: Movie[];
+displayedColumns: string[] =['language','location','plot','poster',
+'soundEffects','stills','title','imdbId','listingType','imbRating','options']
+
+dataSource: MatTableDataSource<Movie>;
+@ViewChild(MatSort,{static:false}) sort: MatSort;
+
+ constructor(
+ ){}
+
+ ngOnInit(): void {
+      this.dataSource = new MatTableDataSource<Movie>();
+      this.dataSource.data = this.searchResults;
+    }
+    ngAfterViewInit(){
+        this.dataSource.sort = this.sort;
+        this.dataSource.sortingDataAccessor =(data, attr) => data[attr]
+    }
+
+    sortData(sort: Sort){
+       this.dataSource.data = this.dataSource.data.sort((a,b) => {
+const isDesc = sort.direction ==='desc';
+switch(sort.active){
+    case 'language': return this.compareData(a.language ,b.language,isDesc);
+    case 'location': return this.compareData(a.location ,b.location,isDesc);
+    case 'title': return this.compareData(a.title ,b.title,isDesc);
+    case 'imdbId': return this.compareData(a.imdbId ,b.imdbId,isDesc);
+    case 'listingType': return this.compareData(a.listingType ,b.listingType,isDesc);
+    case 'imbRating': return this.compareData(a.imbRating ,b.imbRating,isDesc);
+    case 'plot': return this.compareData(a.plot ,b.plot,isDesc);
+}
+       });
+    }
+
+    private compareData(firstValue: string, secondValue: string, isDesc: Boolean) :  number{
+        return (firstValue < secondValue ? 1 : -1) * (isDesc ? -1: 1);
+    }
+}
