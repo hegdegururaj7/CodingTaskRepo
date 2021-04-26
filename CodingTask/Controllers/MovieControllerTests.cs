14309e39f9b1b7cb17a59f85b476a2d68ed3a482
GURURAJ HEGDE <hegdegururaj7@gmail.com>
2021-04-26 15:54:55 +0530

    controller tests , test data json and csproj changes

---
+++ CodingTask/Controllers/MovieControllerTests.cs
@@ -0,0 +1,42 @@
+﻿
+using CodingTask.BusinessLayer;
+using CodingTask.ResponseModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace CodingTask.Controllers
+{
+    public class MovieControllerTests
+    {
+        private readonly Mock<IMovieApi> _movieApi;
+        private readonly Mock<IMovieMetaData> _movieMetaData;
+        private readonly Mock<ILogger<MovieController>> _logger;
+
+        private readonly MovieController _movieControllerTests;
+
+
+        public MovieControllerTests()
+        {
+            _movieApi = new Mock<IMovieApi>();
+            _movieMetaData = new Mock<IMovieMetaData>();
+            _logger = new Mock<ILogger<MovieController>>();
+            _movieControllerTests = new MovieController(_logger.Object,_movieApi.Object, _movieMetaData.Object);
+
+        }
+        [Fact]
+        public void FetchMoviesList_SetsProperties()
+        {
+            _movieMetaData.Setup(x => x.movieList).Returns("testmovie.json");
+            var result =  _movieControllerTests.FetchMoviesList();
+            Assert.IsType<ObjectResult>(result);
+            var movieResponse = Assert.IsAssignableFrom<GetMoviesResponse>(result.Result);
+            var jsonString = System.IO.File.ReadAllText("testmovie.json");
+            var jsonObject = JsonConvert.DeserializeObject<GetMoviesResponse>(jsonString);
+            Assert.Same(jsonObject, movieResponse);
+        }
+
+    }
+}
