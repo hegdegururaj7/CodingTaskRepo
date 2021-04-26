4a66c8fbfdaf9c3cce4e8d98ddd359a5a3dfc2f5
GURURAJ HEGDE <hegdegururaj7@gmail.com>
2021-04-26 18:12:07 +0530

    Setting up test project and moving existing api tests

---
+++ CodingTask.Tests/BusinessLayer/MovieApiTests.cs
@@ -0,0 +1,51 @@
+﻿using CodingTask.BusinessLayer;
+using CodingTask.ResponseModels;
+using Moq;
+using System.Linq;
+using Xunit;
+
+namespace CodingTask.Tests.BusinessLayer
+{
+    public class MovieApiTests
+    {
+        private readonly MovieApi _movieApiTests;
+        private readonly Mock<IMovieFactory> _movieFactory;
+        private readonly Mock<IMovieMetaData> _movieMetaData;
+
+
+        public MovieApiTests()
+        {
+            _movieFactory = new Mock<IMovieFactory>();
+            _movieMetaData = new Mock<IMovieMetaData>();
+            _movieApiTests = new MovieApi(_movieFactory.Object,_movieMetaData.Object);
+
+        }
+        [Fact]
+        public void Search_WhenMovieMetaDataIsNull_ReturnsNull()
+        {
+            const string titleTerm = "som";
+            _movieMetaData.Setup(x => x.movieList).Returns(string.Empty);
+            var result = _movieApiTests.Search(titleTerm);
+            Assert.Null(result);
+            _movieFactory.Verify(x => x.Create(It.IsAny<MovieResponse>()), Times.Never);
+
+        }
+        [Fact]
+        public void Search_WhenSearchedOnTitle_ReturnsResponsesAccordingly()
+        {
+            const string titleTerm = "som";
+            const string notFoundTitle = "not Found Title";
+            _movieMetaData.Setup(x => x.movieList).Returns("testmovie.json");
+            _movieFactory.Setup(x => x.Create(It.IsAny<MovieResponse>())).Returns(new MovieResponse
+            {
+                Id =1,
+                Title= "some title"
+            });
+            var result = _movieApiTests.Search(titleTerm);
+            Assert.Single(result);
+            Assert.NotEqual(notFoundTitle, result.First().Title);
+            _movieFactory.Verify(x => x.Create(It.IsAny<MovieResponse>()), Times.Once);
+        
+        }
+    }
+}
---
+++ CodingTask.Tests/MovieControllerTests.cs
@@ -0,0 +1,66 @@
+﻿
+using CodingTask.BusinessLayer;
+using CodingTask.Controllers;
+using CodingTask.ResponseModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using System.Linq;
+using Xunit;
+
+namespace CodingTask.Tests
+{
+    public class MovieControllerTests
+    {
+        private readonly Mock<IMovieApi> _movieApi;
+        private readonly Mock<IMovieMetaData> _movieMetaData;
+        private readonly Mock<ILogger<MovieController>> _logger;
+
+        private readonly MovieController _movieControllerTests;
+
+
+        public MovieControllerTests()
+        {
+            _movieApi = new Mock<IMovieApi>();
+            _movieMetaData = new Mock<IMovieMetaData>();
+            _logger = new Mock<ILogger<MovieController>>();
+            _movieControllerTests = new MovieController(_logger.Object,_movieApi.Object, _movieMetaData.Object);
+
+        }
+        [Fact]
+        public void FetchMoviesList_SetsProperties()
+        {
+            _movieMetaData.Setup(x => x.movieList).Returns("testmovie.json");
+            var response =  _movieControllerTests.FetchMoviesList();
+            Assert.IsType<ObjectResult>(response.Result);
+            var objectResult = response.Result as ObjectResult;
+            var movieResponse = Assert.IsAssignableFrom<GetMoviesResponse>(objectResult.Value);
+            var jsonString = System.IO.File.ReadAllText("testmovie.json");
+            var jsonObject = JsonConvert.DeserializeObject<GetMoviesResponse>(jsonString);
+            Assert.Equal(jsonObject.Movies.AsEnumerable().First().Title, movieResponse.Movies.AsEnumerable().First().Title);
+        }
+        [Fact]
+        public void Search_VerifiesSearchCalls()
+        {
+            const string term = "some";
+            _movieMetaData.Setup(x => x.movieList).Returns("testmovie.json");
+            _movieApi.Setup(x => x.Search(term)).Returns(new MovieResponse[]
+            {
+                new MovieResponse()
+                {
+                    Title="some title",
+                    Id=100
+                }
+            });
+            var response = _movieControllerTests.Search(term);
+            Assert.IsType<ObjectResult>(response.Result);
+            var objectResult = response.Result as ObjectResult;
+            var movieResponse = Assert.IsAssignableFrom<GetMoviesResponse>(objectResult.Value);
+            var jsonString = System.IO.File.ReadAllText("testmovie.json");
+            var jsonObject = JsonConvert.DeserializeObject<GetMoviesResponse>(jsonString);
+            Assert.Equal(jsonObject.Movies.AsEnumerable().First().Title, movieResponse.Movies.AsEnumerable().First().Title);
+        }
+
+    }
+}
