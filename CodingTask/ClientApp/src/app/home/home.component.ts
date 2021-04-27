3dcbd6b986b4eb23e41b6ee1fdf705704eb80096
GURURAJ HEGDE <hegdegururaj7@gmail.com>
2021-04-27 23:39:33 +0530

    adding slicer filter fix

--- CodingTask/ClientApp/src/app/home/home.component.ts
+++ CodingTask/ClientApp/src/app/home/home.component.ts
@@ -7,29 +7,40 @@ import { SearchResultsComponent } from "src/app/search-results/search-results.co
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls:['./home.component.scss']
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent  {
-  @ViewChild(SearchComponent, {static:false}) searchComponent: SearchComponent;
-    @ViewChild(SearchResultsComponent, {static:false}) searchTableComponent: SearchResultsComponent;
+export class HomeComponent {
+  @ViewChild(SearchComponent, { static: false }) searchComponent: SearchComponent;
+  @ViewChild(SearchResultsComponent, { static: false }) searchTableComponent: SearchResultsComponent;
 
-    private searchResults: Movie[];
-
+  private searchResults: Movie[];
+  private filteredLanguages: string[];
   constructor(
     private movieService: MoviesService
-  ){
+  ) {
 
   }
-  
 
-  onSearch(searchTerms: string){
-    this.movieService.getMoviesList(searchTerms).subscribe(data =>{
-      this.searchComponent.retrievedMoviesSearch = data.map(x=> x.title);
+
+  onSearch(searchTerms: string) {
+    this.movieService.getMoviesList(searchTerms).subscribe(data => {
+      this.searchComponent.retrievedMoviesSearch = data.map(x => x.title);
+      this.filteredLanguages = data.map(x => x.language);
+      this.filteredLanguages = this.filteredLanguages.filter((n, i) => this.filteredLanguages.indexOf(n) === i);
+      this.searchResults = data;
       this.searchTableComponent.dataSource.data = data;
     },
-    (error) =>{
-      throw(error);
-    }
+      (error) => {
+        throw (error);
+      }
     );
+  }
+
+  onFilter(filteredLanguage: string[]) {
+    if(filteredLanguage.length === 0)
+      return;
+    const filteredDataSource =
+      this.searchResults.filter(x => filteredLanguage.find(y => y === x.language));
+    this.searchTableComponent.dataSource.data = filteredDataSource;
   }
 }
